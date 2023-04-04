83e20b1327931b6cc8fcda4df45083a86dd625d0
CSP2020-NOTE4\iwase <kojiko430430@gmail.com>
2023-04-04 13:42:04 +0900

    slight change

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,7 +11,7 @@ export default function Home({ articles }: Props) {
   return (
     <>
       <h1 className="container mx-auto px-10 pt-10 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3">
-        記事一覧
+        記事一覧☆
       </h1>
       <div className="container mx-auto p-10 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 gap-5">
         {articles.map((article,index) => (
